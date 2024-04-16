8566711f4efd6ed036c7f0b1dadfec66482d010a
Floor Terra <floort@gmail.com>
2024-04-16 21:51:16 +0200

    Automatic update

--- update.py
+++ update.py
@@ -8,6 +8,7 @@ DELAY = 30*60 # 30 minutes
 URL = "https://algoritmes.overheid.nl/api/downloads/NLD?filetype=csv" 
 try:
   page_content = urllib.request.urlopen(URL).read()
+  print(page_content)
   open("Algoritmebeschrijvingen.csv", "w").write(page_content)
   print("OK")
 except:
