6e8f7c703371f5c89265e0920d3904f56f0de05a
Floor Terra <floort@gmail.com>
2024-04-16 21:48:14 +0200

    New print function

--- update.py
+++ update.py
@@ -9,9 +9,9 @@ URL = "https://algoritmes.overheid.nl/api/downloads/NLD?filetype=csv"
 try:
   page_content = urllib2.urlopen(URL).read()
   open("Algoritmebeschrijvingen.csv", "w").write(page_content)
-  print "OK"
+  print("OK")
 except:
-  print "FAILED"
+  print("FAILED")
 
 # Try to commit
 os.system("git add .")
